3209f21bf58eeca9516a0c0042b8c640e04f5b73
Kevlar Kanou <kevlarkanou@outlook.com>
2023-03-20 16:17:45 +0800

    add Driver_cbs

--- sim/driver.sv
+++ sim/driver.sv
@@ -5,13 +5,32 @@
 //`include "atm_cell.sv"
 //`include "Utopia.sv"
 
+typedef class Driver;
+
+/////////////////////////////////////////////////////////////////////////////
+// Driver callback class
+// Simple callbacks that are called before and after a cell is transmitted
+// This class has empty tasks, which are used by default
+// A testcase can extend this class to inject new behavior in the driver
+// without having to change any code in the driver
+
+class Driver_cbs;
+   virtual task pre_tx(input Driver drv,
+		               input UNI_cell ucell);
+   endtask : pre_tx
+
+   virtual task post_tx(input Driver drv,
+		                input UNI_cell ucell);
+   endtask : post_tx
+endclass : Driver_cbs
+
 /////////////////////////////////////////////////////////////////////////////
 class Driver;
 
    mailbox gen2drv;	// For cells sent from generator
    event   drv2gen;	// Tell generator when I am done with cell
    vUtopiaRx Rx;	// Virtual interface for transmitting cells
-
+   Driver_cbs cbsq[$];  // Queue of callback objects
    int PortID;
    
    extern function new(input mailbox gen2drv, 
@@ -37,7 +56,6 @@ function Driver::new(input mailbox gen2drv,
    this.PortID  = PortID;
 endfunction : new 
 
-
 //---------------------------------------------------------------------------
 // run(): Run the driver. 
 // Get transaction from generator, send into DUT
@@ -53,14 +71,21 @@ task Driver::run();
    forever begin
       // Read the cell at the front of the mailbox
       gen2drv.peek(ucell);
-
       begin: Tx
-      //ucell.display($psprintf("@%0t: Drv%0d: ", $time, PortID));
-      send(ucell);
-      refmodel(ucell);
-      end: Tx
+	 // Pre-transmit callbacks
+	 foreach (cbsq[i]) begin
+	    cbsq[i].pre_tx(this, ucell);
+	 end
 
-      gen2drv.get(ucell);      // Remove cell from the mailbox
+	 //ucell.display($psprintf("@%0t: Drv%0d: ", $time, PortID));
+	 send(ucell);
+	 
+	 // Post-transmit callbacks
+	 foreach (cbsq[i])
+	   cbsq[i].post_tx(this, ucell);
+      end : Tx
+
+      gen2drv.get(ucell);  // Remove cell from the mailbox
       ->drv2gen;	       // Tell the generator we are done with this cell
    end
 endtask : run
@@ -82,12 +107,19 @@ task Driver::send(input UNI_cell ucell);
        $write("%x ", pkt_gen.Mem[i]); $display;
    
    // Iterate through bytes of cell, deasserting Start Of Cell indicater
+   //@(Rx[PortID].cbr);
    @(Rx.cbr);
+   //Rx[PortID].cbr.clav <= 1;
    Rx.cbr.clav <= 1;
    for (int i=0; i<=52; i++) begin
+        //while (Rx[PortID].cbr.en === 1'b0) 
         while (Rx.cbr.en === 1'b0) 
+		   //@(Rx[PortID].cbr);
 		   @(Rx.cbr);
 		
+        //Rx[PortID].cbr.soc  <= (i == 0);
+        //Rx[PortID].cbr.data <= pkt_gen.Mem[i];
+        //@(Rx[PortID].cbr);
         Rx.cbr.soc  <= (i == 0);
         Rx.cbr.data <= pkt_gen.Mem[i];
         @(Rx.cbr);
@@ -98,6 +130,9 @@ task Driver::send(input UNI_cell ucell);
    Rx.cbr.soc  <= 0    ;
    Rx.cbr.data <= 8'b0 ;
    Rx.cbr.clav <= 1    ;
+   //Rx[PortID].cbr.soc  <= 0    ;
+   //Rx[PortID].cbr.data <= 8'b0 ;
+   //Rx[PortID].cbr.clav <= 1    ;
    
 endtask
 
